a31654fe9a5c1f37c2db652fb4ba620dc88bfbc2
nguyephi <pronewgen92@gmail.com>
2024-07-02 15:45:59 -0400

    create title and button atom component

---
+++ src/components/atoms/Title.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import clsx from 'clsx';
+import { poppinsFont } from '@/lib/fonts';
+
+interface TitleProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Title: React.FC<TitleProps> = ({ children, className = '' }) => {
+  return (
+    <h1 className={clsx(
+        'text-6xl font-semibold text-white drop-shadow-md', 
+        poppinsFont.className, 
+        className
+        )}>
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
