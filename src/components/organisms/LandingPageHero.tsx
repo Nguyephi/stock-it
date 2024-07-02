07978e63a5e702c83214191b74111bc9ba89811d
nguyephi <pronewgen92@gmail.com>
2024-07-02 18:43:51 -0400

    update component name

---
+++ src/components/organisms/LandingPageHero.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import Title from "../atoms/Title";
+import Button from "../atoms/Button";
+import { useRouter } from "next/navigation";
+ 
+export function LandingPageHero() {
+  const router = useRouter();
+
+  const handleSignin = () => {
+    router.push("/auth/register");
+  }
+
+  return (
+    <div className="space-y-6 text-center">
+        <Title>Welcome to the landing page!</Title>
+        <Button type="button" onClick={handleSignin} size='small' variant='primary'>Sign in here</Button>
+    </div>
+  )
+}
