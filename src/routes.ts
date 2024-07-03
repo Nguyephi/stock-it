a026e51a9288662d44106fa92ef56b6521674389
nguyephi <pronewgen92@gmail.com>
2024-07-03 18:47:00 -0400

    add route with descriptions

---
+++ src/routes.ts
@@ -0,0 +1,31 @@
+/**
+ * Public routes.
+ * No auth needed
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/",
+]
+
+/**
+ * Auth routes used for auth.
+ * Redirects to logged in users to /dashboard
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/signin",
+    "/auth/register",
+]
+
+/**
+ * Prefix for api auth routes.
+ * Routes with this prefix are used for auth
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * Default redirect path after user logs in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard"
