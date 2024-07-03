2421d3f14cc09a163ff86af48a5a8e6fad4b5764
nguyephi <pronewgen92@gmail.com>
2024-07-03 18:49:55 -0400

    update schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -9,7 +9,7 @@ export const RegisterSchema = z.object({
     username: z.string().min(1, 'Username is required'),
     email: z.string().min(1, 'Email is required').email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
-    confirmPassword: z.string().min(6, 'Confirm Password is required')
+    confirmPassword: z.string()
   }).refine(data => data.password === data.confirmPassword, {
     message: 'Passwords do not match',
     path: ['confirmPassword'],
