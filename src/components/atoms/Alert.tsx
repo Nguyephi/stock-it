9e17e32e06a5fb232aa83cb406a90d0c4eade4a6
nguyephi <pronewgen92@gmail.com>
2024-07-03 18:43:33 -0400

    creat alert

---
+++ src/components/atoms/Alert.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import clsx from 'clsx';
+
+interface AlertProps {
+    message?: string;
+    icon?: React.ReactNode;
+    iconPlacement?: "before" | "after"
+    className?: string;
+}
+
+
+const Alert: React.FC<AlertProps> = ({ message, icon, iconPlacement, className }) => {
+    if (!message) return null;
+    return (
+        <div role="alert" className={clsx('alert rounded-md text-sm', className)}>
+            {iconPlacement === "before" && icon}
+            <span>{message}</span>
+            {iconPlacement === "after" && icon}
+        </div>
+    );
+};
+
+export default Alert;
