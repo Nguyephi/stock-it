8f01b100c911a3cc0cb411137edcde83fa83f77b
nguyephi <pronewgen92@gmail.com>
2024-07-04 12:39:10 -0400

    add clsx

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -1,14 +1,16 @@
 "use client"
 
+import clsx from 'clsx';
 import React from 'react';
 
 interface CardProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Card: React.FC<CardProps> = ({ children }) => {
+const Card: React.FC<CardProps> = ({ children, className="" }) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">{children}</div>
+    <div className={clsx("card bg-base-100 w-96 shadow-xl", className)}>{children}</div>
   );
 };
 
--- src/components/atoms/CardHeader.tsx
+++ src/components/atoms/CardHeader.tsx
@@ -1,14 +1,16 @@
 "use client"
 
+import clsx from 'clsx';
 import React from 'react';
 
 interface CardHeaderProps {
-  children: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-const CardHeader: React.FC<CardHeaderProps> = ({ children }) => {
+const CardHeader: React.FC<CardHeaderProps> = ({ children, className = "" }) => {
   return (
-      <h1 className="card-title block text-xl font-semibold">{children}</h1>
+      <h1 className={clsx("card-title block text-xl font-semibold", className)}>{children}</h1>
   );
 };
 
