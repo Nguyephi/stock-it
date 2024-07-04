dc3103e3de6c1e2f193d90d5a5d00d4e342c9805
nguyephi <pronewgen92@gmail.com>
2024-07-04 12:42:26 -0400

    send email verification

---
+++ src/components/organisms/NewVerificationForm.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import React, { useCallback, useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { BeatLoader } from 'react-spinners';
+import { FaCheckCircle } from 'react-icons/fa';
+import { FiAlertTriangle } from 'react-icons/fi';
+
+import { newVerification } from '@/actions/new-verification';
+import Alert from '../atoms/Alert';
+import AuthCard from '../molecules/AuthCard';
+
+export default function NewVerificationForm() {
+    const searchParams = useSearchParams();
+    const token = searchParams.get("token")
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
+
+    const onSubmit = useCallback(() => {
+        if (!token) {
+            setError("Missing token!")
+            return
+        }
+        newVerification(token)
+            .then((data) => {
+                setSuccess(data.success)
+                setError(data.error)
+            })
+            .catch(() => {
+                setError("Something went wrong!")
+            })
+    }, [token])
+
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit]);
+
+    return (
+        <div>
+            <AuthCard
+                headerLabel='Verify account'
+                backButtonLabel="Back to login"
+                backButtonHref="/auth/sigin"
+            >
+                {!success && !error && (
+                    <div className='flex w-full justify-center'>
+                        <BeatLoader />
+                    </div>
+                )}
+                <Alert icon={<FaCheckCircle className="h-4 w-4" />} iconPlacement="before" message={success} className='text-green-950 alert-success mt-4' />
+                <Alert icon={<FiAlertTriangle className="h-4 w-4" />} iconPlacement="before" message={error} className='text-red-950 alert-error mt-4' />
+            </AuthCard>
+        </div>
+    )
+}
