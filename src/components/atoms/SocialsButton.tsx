13bc82196106d08591771ea5923c8e2ac70d42cd
nguyephi <pronewgen92@gmail.com>
2024-07-04 12:39:27 -0400

    fb oauth sign

--- src/components/atoms/SocialsButton.tsx
+++ src/components/atoms/SocialsButton.tsx
@@ -2,8 +2,15 @@
 
 import { FaGoogle, FaFacebook } from "react-icons/fa"
 import Button from "./Button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Socials: React.FC = () => {
+    const onClick = (provider: "facebook") => {
+        signIn(provider, {
+            callbaclUrl: DEFAULT_LOGIN_REDIRECT
+        })
+    } 
     return (
         <div className="flex justify-center items-center space-x-2">
             <Button
@@ -14,7 +21,7 @@ const Socials: React.FC = () => {
             </Button>
             <Button
                 className="min-w-[50%] text-sm btn-outline"
-                onClick={() => {}}
+                onClick={() => onClick('facebook')}
             >
                 <FaFacebook className="h-4 w-4" />
             </Button>
