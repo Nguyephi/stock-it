df212b7ce1712e183bcb32d7f0d7056e98926c96
nguyephi <pronewgen92@gmail.com>
2024-07-04 20:02:45 -0400

    clean up dashboard

--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,19 +1,15 @@
 'use client'
 
-import React, { useEffect } from 'react';
-import SignOut from '../organisms/SignOut';
-import { useSession } from 'next-auth/react';
+import React from 'react';
+import SignOut from '../molecules/SignOut';
+import { useAppSelector } from '@/redux/store';
 
 const DashboardPage = () => {
-    const session = useSession();
-    useEffect(() => {
-        console.log("dashboard", session);
-        
-    }, [])
+    const user = useAppSelector(state => state.user.data);
     return (
         <div className="flex h-full flex-col items-center justify-center bg-sky-400">
             <SignOut />
-            {JSON.stringify(session)}
+            {user && JSON.stringify(user)}
         </div>
     )
 };
