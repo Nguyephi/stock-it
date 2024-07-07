c37bb09118a9af3ec5dab08f796d5a7836f058fe
nguyephi <pronewgen92@gmail.com>
2024-07-07 00:55:44 -0400

    update card on error page

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
-import Card from '../atoms/card'
-import CardBody from '../atoms/CardBody'
-import Title from '../atoms/Title'
-import CardActions from '../atoms/CardActions'
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardTitle,
+    CardHeader
+} from '../atoms/card';
 import LinkButton from '../atoms/LinkButton'
-import CardHeader from '../atoms/CardHeader'
+import Divider from '../atoms/Divider'
 
 const ErrorPage = () => {
     return (
-        <Card className="w-[450px]">
-            <CardBody>
-                <Title>💩</Title>
-                <CardHeader className="text-red-700 text-2xl">Oops! Something went wrong</CardHeader>
-                <CardActions className="justify-center">
+        <main>
+            <Card className="w-[450px]">
+                <CardHeader className="flex flex-col space-y-2 items-cente">
+                    <CardTitle>💩</CardTitle>
+                    <CardDescription className="text-red-700 text-2xl">Oops! Something went wrong</CardDescription>
+                </CardHeader>
+                <CardFooter className="flex flex-col space-y-2">
+                    <Divider orientation="horizontal" />
                     <LinkButton
                         label="Back to login"
                         href="/auth/signin"
                     />
-                </CardActions>
-            </CardBody>
-        </Card>
+                </CardFooter>
+            </Card>
+        </main>
     )
 }
 
