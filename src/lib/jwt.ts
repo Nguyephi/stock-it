34574f31bcfa413f3080bd9e20f6717671130c96
nguyephi <pronewgen92@gmail.com>
2024-07-09 13:46:43 -0400

    add jwt

---
+++ src/lib/jwt.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+const secret = process.env.JWT_SECRET;
+
+if (!secret) {
+  throw new Error('Missing JWT_SECRET environment variable');
+}
+
+interface TokenPayload {
+    token: string;
+}
+
+export const encryptToken = (token: string): string => {
+    return jwt.sign({ token }, secret, { expiresIn: '3y' });
+};
+
+export const decryptToken = (token: string): string => {
+    try {
+        const decoded = jwt.verify(token, secret) as TokenPayload;
+        return decoded.token;
+    } catch (error) {
+        throw new Error('Invalid token');
+    }
+};
