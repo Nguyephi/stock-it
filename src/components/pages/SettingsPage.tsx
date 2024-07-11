e7aff34aeb0ebbbd9d3e6928474bf1bcb47a9cf1
nguyephi <pronewgen92@gmail.com>
2024-07-11 10:35:58 -0400

    edit setting page

--- src/components/pages/SettingsPage.tsx
+++ src/components/pages/SettingsPage.tsx
@@ -6,28 +6,30 @@ import DashboardLayout from '../templates/DashboardLayout';
 import { storePrintifyAccessToken } from '@/actions/printify/access-token';
 import Title from '../atoms/Title';
 import ShopConnectCard from '../molecules/ShopConnectCard';
+import Divider from '../atoms/Divider';
 
 const SettingsPage = () => {
     const { data: user } = useUserStore();
     return (
         <DashboardLayout>
-            <Title>Welcome to the Settings Page</Title>
-            <div className='container flex w-full space-x-4'>
-            <ShopConnectCard
-                provider="printify"
-                headerLabel="Connect your printify"
-                description="Grab your personal access token from your printify account and paste it here."
-                handleSubmit={(values) => storePrintifyAccessToken(values)}
-            >
-            </ShopConnectCard>
-            <ShopConnectCard
-                provider="etsy"
-                headerLabel="Connect your etsy"
-                description="Add input and button here"
-            >
-            </ShopConnectCard>
+            <Title className='text-left w-full px-8 pt-8 pb-4'>Settings</Title>
+            <Divider className='w-full mb-8' />
+            <div className='container flex w-full space-x-4 py-4'>
+                <ShopConnectCard
+                    provider="printify"
+                    headerLabel="Connect your printify"
+                    description="Grab your personal access token from your printify account and paste it here."
+                    handleSubmit={(values) => storePrintifyAccessToken(values)}
+                >
+                </ShopConnectCard>
+                <ShopConnectCard
+                    provider="etsy"
+                    headerLabel="Connect your etsy"
+                    description="Add input and button here"
+                >
+                </ShopConnectCard>
             </div>
-        </DashboardLayout>  
+        </DashboardLayout>
     )
 };
 
