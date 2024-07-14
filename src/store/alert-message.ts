20c22d67b3b74f969e4742d0e7ccc1013f1d18eb
nguyephi <pronewgen92@gmail.com>
2024-07-14 11:03:34 -0400

    update alert store

--- src/store/alert-message.ts
+++ src/store/alert-message.ts
@@ -3,17 +3,21 @@ import { create } from 'zustand';
 interface AlertState {
     error: string | undefined;
     success: string | undefined;
+    provider: string | undefined;
     setError: (error: string | undefined) => void;
     setSuccess: (success: string | undefined) => void;
+    setProvider: (provider: string) => void;
     clearMessages: () => void;
 }
 
 const useAlertStore = create<AlertState>((set) => ({
     error: undefined,
     success: undefined,
+    provider: undefined,
     setError: (error) => set({ error }),
     setSuccess: (success) => set({ success }),
-    clearMessages: () => set({ error: undefined, success: undefined }),
+    setProvider: (provider) => set({ provider }),
+    clearMessages: () => set({ error: undefined, success: undefined, provider: undefined }),
 }));
 
 export default useAlertStore;
