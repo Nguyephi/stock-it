d46077d22df4692d91b2254ec4c74b65b84061a8
nguyephi <pronewgen92@gmail.com>
2024-07-15 20:29:57 -0400

    format main layout

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -6,38 +6,39 @@ import Footer from '../atoms/Footer';
 import { Button } from "../atoms/button";
 import { useRouter } from 'next/navigation';
 
-interface DashboardLayoutProps {
+interface MainLayoutProps {
     children: React.ReactNode;
 }
 
-const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+const MainLayout = ({ children }: MainLayoutProps) => {
     const router = useRouter();
 
-  const handleSignin = () => {
-    router.push("/auth/signin");
-  }
+    const handleSignin = () => {
+        router.push("/auth/signin");
+    }
     return (
-    <div className='flex min-h-screen flex-col items-center justify-center bg-sky-400'>
-        <AppBar 
-        leftComponent={
-            <div>
-                Logo
+        <div className='flex min-h-screen flex-col items-center justify-center bg-sky-400'>
+            <AppBar
+                leftComponent={
+                    <div>
+                        Logo
+                    </div>
+                }
+                rightComponent={
+                    <div>
+                        <Button type="button" onClick={handleSignin} variant="ghost" size="sm" className="text-md">Sign in</Button>
+                    </div>
+                } />
+            <div className="container flex-grow pt-16 pb-16 w-full">
+                <main className="flex flex-col justify-center items-center">
+                    {children}
+                </main>
             </div>
-            } 
-            rightComponent={
-            <div>
-                <Button type="button" onClick={handleSignin} variant="ghost" size="sm" className="text-md">Sign in</Button>
-            </div>
-            } />
-        <div className="container flex-grow pt-16 pb-16 w-full">
-            <main className="flex flex-col justify-center items-center">
-                {children}
-            </main>
+            <Footer>
+                <div>Footer</div>
+            </Footer>
         </div>
-        <Footer>
-            <div>Footer</div>
-        </Footer>
-    </div>
-)};
+    )
+};
 
-export default DashboardLayout;
+export default MainLayout;
