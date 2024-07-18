9587e595c9d56af33d936024172de9cb0a1760ef
nguyephi <pronewgen92@gmail.com>
2024-07-18 11:51:34 -0400

    add etsy server action

--- src/actions/etsy/access-token.ts
+++ src/actions/etsy/access-token.ts
@@ -1,8 +1,29 @@
 "use server"
 
 import { auth } from "@/auth";
-import { getEtsyAccessTokenByUserId, getEtsyDataByUserId } from "@/data/etsy";
+import { getEtsyAccessTokenByUserId, getEtsyShopDataByUserId } from "@/data/etsy";
 import { db } from "@/lib/db";
+
+/**
+ * initiates Etsy OAuth
+ * @returns
+ */
+export const handleEtsyOauth = async () => {
+    const session = await auth();
+    const userId = session?.user?.id;
+    if (!userId) {
+        return { error: "User is not authenticated!" }
+    }
+    try {
+        const response = await fetch('/api/auth/etsy/connect');
+        const data = await response.json();
+        window.location.href = new URL(data.authorizationUrl).toString();
+        return
+    } catch (error) {
+        console.error('Error initiating Etsy OAuth:', error);
+        return { error: 'Something went wrong!' }
+    }
+}
 
 export const getEtsyToken = async () => {
     const session = await auth();
@@ -22,6 +43,24 @@ export const getEtsyToken = async () => {
     }
 }
 
+export const getEtsyShopData = async () => {
+    const session = await auth();
+    const userId = session?.user?.id;
+    if (!userId) {
+        return { error: "User is not authenticated!" }
+    }
+
+    try {
+        const etsy = await getEtsyShopDataByUserId(userId);
+        if (!etsy) {
+            return { error: 'No access token found' };
+        }
+        return etsy;
+    } catch {
+        return null;
+    }
+}
+
 export const deleteEtsyData = async () => {
     const session = await auth();
     const userId = session?.user?.id;
@@ -30,7 +69,8 @@ export const deleteEtsyData = async () => {
     }
 
     try {
-        const etsy = await getEtsyDataByUserId(userId);
+        // delete from account and etsy tables
+        const etsy = await getEtsyShopDataByUserId(userId);
 
         if (!etsy) {
             return { error: 'No access token found' };
